f4a97b0cf28f7c44e53c1c6d5d4716893139b6fd
郑龙天 <zhenglongtian@outlook.com>
2018-04-07 14:56:51 +0800

    正确率可以稳步上升，但是好慢
    我记得之前有一次不知道怎么就跑到90了，现在不知道怎么弄才可以

--- orgin_fomula/Emotionid_new.py
+++ orgin_fomula/Emotionid_new.py
@@ -11,7 +11,7 @@ import xgboost as xgb
 from data_getter import get_next_train_batch, get_next_test_batch, get_feature_number, get_batch_size, get_n_step
 
 
-def i_want_to_see(title, content=[]):
+def i_want_to_see(title='title', content=[]):
     '''
     print what you want to see in a cool way
     @param name:
@@ -28,33 +28,31 @@ def RNN(X):
 
     # neurons in hidden layer
     input_units = n_inputs
-    fc1_in_size = 128
-    fc1_nuits = 64
-    fc2_nuits = 64
-    fc3_nuits = 64
-    fc4_nuits = 64
-    fc5_nuits = 64
+    fc1_in_size = input_units
+    fc1_nuits = input_units
+    fc2_nuits = input_units
+    fc3_nuits = 256
+    fc4_nuits = 256
+    fc5_nuits = 256
 
     # Define weights and biases
     weights = {
         'input': tf.Variable(tf.random_normal([n_inputs, input_units]), trainable=True),
-        'lstm_out': tf.Variable(tf.random_normal([input_units, fc1_in_size]), trainable=True),
         'fc1': tf.Variable(tf.random_normal([fc1_in_size, fc1_nuits]), trainable=True),
         'fc2': tf.Variable(tf.random_normal([fc1_nuits, fc2_nuits]), trainable=True),
-        'fc3': tf.Variable(tf.random_normal([fc2_nuits, fc3_nuits]), trainable=True),
-        'fc4': tf.Variable(tf.random_normal([fc3_nuits, fc4_nuits]), trainable=True),
-        'fc5': tf.Variable(tf.random_normal([fc4_nuits, fc5_nuits]), trainable=True),
-        'output': tf.Variable(tf.random_normal([fc5_nuits, n_classes]), trainable=True),
+        'lstm_out': tf.Variable(tf.random_normal([fc2_nuits, n_classes]), trainable=True),
+        # 'fc3': tf.Variable(tf.random_normal([fc2_nuits, fc3_nuits]), trainable=True),
+        # 'fc4': tf.Variable(tf.random_normal([fc3_nuits, fc4_nuits]), trainable=True),
+        # 'output': tf.Variable(tf.random_normal([fc1_nuits, n_classes]), trainable=True),
     }
     biases = {
         'input': tf.Variable(tf.constant(0.1, shape=[input_units]), trainable=True),
-        'lstm_out': tf.Variable(tf.constant(0.1, shape=[fc1_in_size]), trainable=True),
         'fc1': tf.Variable(tf.constant(0.1, shape=[fc1_nuits]), trainable=True),
         'fc2': tf.Variable(tf.constant(0.1, shape=[fc2_nuits]), trainable=True),
-        'fc3': tf.Variable(tf.constant(0.1, shape=[fc3_nuits]), trainable=True),
-        'fc4': tf.Variable(tf.constant(0.1, shape=[fc4_nuits]), trainable=True),
-        'fc5': tf.Variable(tf.constant(0.1, shape=[fc5_nuits]), trainable=True),
-        'output': tf.Variable(tf.constant(0.1, shape=[n_classes]), trainable=True),
+        'lstm_out': tf.Variable(tf.constant(0.1, shape=[n_classes]), trainable=True),
+        # 'fc3': tf.Variable(tf.constant(0.1, shape=[fc3_nuits]), trainable=True),
+        # 'fc4': tf.Variable(tf.constant(0.1, shape=[fc4_nuits]), trainable=True),
+        # 'output': tf.Variable(tf.constant(0.1, shape=[n_classes]), trainable=True),
     }
 
     ###################################### model struct srart ############################################
@@ -63,14 +61,28 @@ def RNN(X):
     # X (batch_size,n_step,input_units=n_inputs)===>X (?,input_units=n_inputs)
     X = tf.reshape(X, [-1, input_units])
 
-    X_hidd1 = tf.nn.relu(
+    input_layer = tf.nn.relu(
         tf.add(
             tf.matmul(X, weights['input']),
             biases['input']))
 
-    # 2------------------- lstm -------------------
+    # 2------------------- fc -------------------
+    # X (input_units, fc1_in_size)
+    fc1_layer = tf.nn.relu(
+        tf.add(
+            tf.matmul(input_layer, weights['fc1']),
+            biases['fc1']))
+
+    # 3------------------- fc -------------------
+    # X (input_units, fc1_in_size)
+    fc2_layer = tf.nn.relu(
+        tf.add(
+            tf.matmul(fc1_layer, weights['fc2']),
+            biases['fc2']))
+
+    # 4------------------- lstm -------------------
     # X_hidd1 (?,input_units=n_inputs) ====> X_in (batch_size=?,n_step,input_units=n_inputs)
-    X_in = tf.reshape(X_hidd1, [-1, n_step, input_units])
+    X_in = tf.reshape(fc2_layer, [-1, n_step, input_units])
 
     lstm_cell = tf.contrib.rnn.BasicLSTMCell(
         input_units, forget_bias=1, state_is_tuple=True)
@@ -80,40 +92,23 @@ def RNN(X):
     outputs, final_state = tf.nn.dynamic_rnn(
         lstm_cell, X_in, initial_state=init_state, time_major=False)
 
-    # outputs , final_states is the last outputs
     outputs = tf.unstack(tf.transpose(outputs, [1, 0, 2]))
     lstm_results = tf.matmul(outputs[-1], weights['lstm_out']) + \
         biases['lstm_out']  # 选取最后一个 output
 
-    # 3------------------- fc -------------------
-    # X (input_units, fc1_in_size)
+    # fc3_layer = tf.nn.tanh(
+    #     tf.add(
+    #         tf.matmul(fc2_layer, weights['fc3']),
+    #         biases['fc3']))
+    # fc4_layer = tf.nn.sigmoid(
+    #     tf.add(
+    #         tf.matmul(fc3_layer, weights['fc4']),
+    #         biases['fc4']))
 
-    fc1_layer = tf.nn.relu(
-        tf.add(
-            tf.matmul(lstm_results, weights['fc1']),
-            biases['fc1']))
-    fc2_layer = tf.nn.elu(
-        tf.add(
-            tf.matmul(fc1_layer, weights['fc1']),
-            biases['fc1']))
+    # results = tf.nn.relu(tf.matmul(fc1_layer, weights['output']) +
+    #                      biases['output'])
 
-    fc3_layer = tf.nn.tanh(
-        tf.add(
-            tf.matmul(fc2_layer, weights['fc1']),
-            biases['fc1']))
-    fc4_layer = tf.nn.sigmod(
-        tf.add(
-            tf.matmul(fc3_layer, weights['fc1']),
-            biases['fc1']))
-    fc5_layer = tf.nn.relu(
-        tf.add(
-            tf.matmul(fc4_layer, weights['fc1']),
-            biases['fc1']))
-
-    results = tf.nn.relu(tf.matmul(fc5_layer, weights['output']) +
-                         biases['output'])
-
-    return lstm_results  # , outputs_att
+    return lstm_results  # results  # , outputs_att
 
 
 ##################################################################### 跑模型 ##################################################################
@@ -133,7 +128,7 @@ batch_size = get_batch_size()
 lameda = 0.001
 train_times = 50000
 
-learning_rate = 0.001
+learning_rate = 0.0001
 # tf.train.exponential_decay(
 #     learning_rate=0.01,
 #     global_step=train_times,
@@ -168,15 +163,16 @@ accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 # confusion_matrix=tf.contrib.metrics.confusion_matrix(pred_result,label_true)
 
-summary_loss = tf.summary.scalar("loss", cost)
-summary_accuracy = tf.summary.scalar("accuracy", accuracy)
+summary_loss = tf.summary.scalar("train_loss", cost)
+summary_accuracy = tf.summary.scalar("train_accuracy", accuracy)
 
 streaming_loss, streaming_loss_update = tf.contrib.metrics.streaming_mean(cost)
-streaming_loss_scalar = tf.summary.scalar('loss', streaming_loss)
+streaming_loss_scalar = tf.summary.scalar('test_loss', streaming_loss)
 
 streaming_accuracy, streaming_accuracy_update = tf.contrib.metrics.streaming_mean(
     accuracy)
-streaming_accuracy_scalar = tf.summary.scalar('accuracy', streaming_accuracy)
+streaming_accuracy_scalar = tf.summary.scalar(
+    'test_accuracy', streaming_accuracy)
 
 train_merge = tf.summary.merge([summary_loss, summary_accuracy])
 test_merge = tf.summary.merge(
@@ -193,11 +189,14 @@ with tf.Session(config=config) as sess:
     test_writer = tf.summary.FileWriter(logfile+"/test", sess.graph)
 
     lr = learning_rate  # sess.run(learning_rate)
+    test_x, test_y = get_next_test_batch()
 
     for step in range(train_times):
 
         ################ train #################
         batch_x, batch_y = get_next_train_batch()
+
+        # i_want_to_see('train_data',content=[batch_x,batch_y])
 
         _, train_accuracy, train_cost, summary = sess.run([train_op, accuracy, cost, train_merge], feed_dict={
             x: batch_x,
@@ -205,16 +204,15 @@ with tf.Session(config=config) as sess:
         })
 
         train_writer.add_summary(summary, step)
-        sess.run(tf.local_variables_initializer())
 
         ############### print something ##################
-        if step % 10 == 0:
+        sess.run(tf.local_variables_initializer())
 
-            batch_x, batch_y = get_next_test_batch()
+        if step % 20 == 0:
 
             test_accuracy, streaming_loss, streaming_accuracy = sess.run([accuracy, streaming_loss_update, streaming_accuracy_update],
-                                                                         feed_dict={x: batch_x,
-                                                                                    y: batch_y,
+                                                                         feed_dict={x: test_x,
+                                                                                    y: test_y,
                                                                                     })
             summary = sess.run(test_merge)
             test_writer.add_summary(summary, step)
